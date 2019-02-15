fd416ee97ef89ae03e3550f8073786a042cb8601
johnpharmd <jhumphre@uw.edu>
2019-02-15 10:24:59 -0800

    Worked further toward SC req's for Part 4, but will likely not fully meet req's

--- acme_report.py
+++ acme_report.py
@@ -8,17 +8,30 @@ import random
 # Please note: the specs for this part of the SC appear to be best met
 # using functional programming
 
-adject_list = []
-price_rand = 0
-weight_rand = 0
-flamm_rand = 0.0
+prod_list = []
+adject_list = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+noun_list = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
+int_rand = random.randrange(5, 101)  # use for both price and weight
+flamm_rand = random.randrange(0.0, 2.51)
 
 
-def generate_products():
-    pass
+def generate_products(num_prods=30):
+    '''
+    This function generates a list of a specified number of products
+    '''
+    for _ in range(num_prods):
+        prod_list.append([random.choice(adject_list),
+                         random.choice(noun_list), int_rand, int_rand,
+                         flamm_rand])
+    return prod_list
 
 
 def inventory_report():
+    '''
+    This function calculates and prints number of unique product names
+    in the product list; and mean price, weight, and flammability of
+    listed products
+    '''
     pass
 
 
