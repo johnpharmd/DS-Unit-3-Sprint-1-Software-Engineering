c3887b7ed0fa96694cc8e8f52c71635aeb406934
johnpharmd <johnpharmd@users.noreply.github.com>
2019-02-15 08:37:25 -0800

    Write class 'Product' for acme.py

---
+++ acme.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import random
+
+
+class Product():
+    def __init__(self, name='', price=10, weight=20, flammability=0.5, identifier=random.randrange(int(1e6), int(1e9))):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
