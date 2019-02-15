5efc620d123b24977249031d139839cb065dc54b
johnpharmd <jhumphre@uw.edu>
2019-02-15 10:03:50 -0800

    Add imports, new vars, and 2 func stubs to acme_report.py

---
+++ acme_report.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+This module uses acme.py to generate random products then print a summary
+'''
+from acme import Product
+import random
+
+# Please note: the specs for this part of the SC appear to be best met
+# using functional programming
+
+adject_list = []
+price_rand = 0
+weight_rand = 0
+flamm_rand = 0.0
+
+
+def generate_products():
+    pass
+
+
+def inventory_report():
+    pass
+
+
