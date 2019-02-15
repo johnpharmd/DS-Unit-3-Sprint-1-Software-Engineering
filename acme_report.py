39e710f18a2d25522541e02dad428119fdf08a22
johnpharmd <jhumphre@uw.edu>
2019-02-15 15:11:52 -0800

    Refactor Part 4 multiple times

--- acme_report.py
+++ acme_report.py
@@ -4,6 +4,7 @@ This module uses acme.py to generate random products, then prints a summary
 '''
 from acme import Product
 import random
+from statistics import mean
 
 # Please note: the specs for this part of the SC appear to be best met
 # using functional programming
@@ -11,19 +12,28 @@ import random
 prod_list = []
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
-int_rand = random.randrange(5, 101)  # use for both price and weight
+price_list = [_ for _ in range(5, 101)]
+weight_list = [_ for _ in range(5, 101)]
 flamm_rand = random.uniform(0.0, 2.51)
+
+
+def get_flamm_rand():
+    return flamm_rand
 
 
 def generate_products(num_prods=30):
     '''
     This function generates a list of a specified number of products
     '''
+    product_list = []
     for _ in range(num_prods):
-        prod_list.append([random.choice(ADJECTIVES) + ' ' +
-                         random.choice(NOUNS), int_rand, int_rand,
-                         flamm_rand])
-    return prod_list
+        gen_list = []
+        gen_list = [random.choice(ADJECTIVES) + ' ' +
+                    random.choice(NOUNS), random.choice(price_list),
+                    random.choice(weight_list)]
+        gen_list.append(get_flamm_rand())
+        product_list.append(gen_list)
+    return product_list
 
 
 def inventory_report(product_list):
@@ -32,11 +42,11 @@ def inventory_report(product_list):
     in the product list; and mean price, weight, and flammability of
     listed products
     '''
-    product_set = set(product_list)
-    product_set = (lambda n: n[0], product_set)
-    mean_price = mean(lambda x: x[1], product_set)
-    mean_weight = mean(lambda y: y[2], product_set)
-    mean_flam = mean(lambda z: z[3], product_set)
+    # product_set = set(product_list)
+    # product_list = []
+    mean_price = []
+    mean_weight = []
+    mean_flam = []
 
     report_text = '''
     ACME CORPORATION OFFICIAL INVENTORY REPORT
@@ -45,8 +55,9 @@ def inventory_report(product_list):
     Average weight: {}
     Average flammability: {}
     '''
-    print(report_text.format(len(product_set), mean_price,
-                             mean_weight, mean_flam))
+    # print(report_text.format(len(product_list), mean_price,
+    #                           mean_weight, mean_flam))
+    print('product_list is:', product_list)
 
 if __name__ == '__main__':
     inventory_report(generate_products())
