7a1fbef0201ca5678a2095fb63d94bc5f2f8c125
johnpharmd <jhumphre@uw.edu>
2019-02-15 08:55:26 -0800

    Refactor [again] identifier kwarg

--- acme.py
+++ acme.py
@@ -3,7 +3,7 @@ import random
 
 
 class Product():
-    def __init__(self, name='', price=10, weight=20, flammability=0.5, identifier=random.randrange(int(1e7), int(1e8))):
+    def __init__(self, name='', price=10, weight=20, flammability=0.5, identifier=random.randrange(int(1e7))):
         self.name = name
         self.price = price
         self.weight = weight
